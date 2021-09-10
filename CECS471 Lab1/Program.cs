b5b404fa0786c3f6869b824f316227acb46b62aa
tealaurey <Tea.Laurey@student.csulb.edu>
2021-09-10 09:23:38 -1000

    testing stuff
    right now this is giving me stack overflow errors

--- CECS471 Lab1/Program.cs
+++ CECS471 Lab1/Program.cs
@@ -1,19 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using System.Threading;
+
 namespace Stock
 {
     public class Stock
     {
         public event EventHandler<StockNotification> StockEvent;
+
         private readonly Thread _thread;
-        public string StockName { get; set; }
-        public int InitialValue { get; set; }
-        public int CurrentValue { get; set; }
-        public int MaxChange { get; set; }
-        public int Threshold { get; set; }
-        public int NumChanges { get; set; }
+        public string StockName { get => StockName; set => StockName = value; }
+        public int InitialValue { get => InitialValue; set => InitialValue = value; }
+        public int CurrentValue { get => CurrentValue; set => CurrentValue = value; }
+        public int MaxChange { get => MaxChange; set => MaxChange = value; }
+        public int Threshold { get => Threshold; set => Threshold = value; }
+        public int NumChanges { get => NumChanges; set => NumChanges = value; }
+
         /// <summary>
         /// Stock class that contains all the information and changes of the stock
         /// </summary>
@@ -23,8 +27,18 @@ namespace Stock
         /// <param name="threshold">The range for the stock</param>
         public Stock(string name, int startingValue, int maxChange, int threshold)
         {
+            StockName = name;
+            InitialValue = startingValue;
+            MaxChange = maxChange;
+            Threshold = threshold;
+            NumChanges = 0;
+            CurrentValue = startingValue;
 
+            ThreadStart threadRef = new ThreadStart(Activate);
+            Thread stockThread = new Thread(threadRef);
+            stockThread.Start();
         }
+
         /// <summary>
         /// Activates the threads synchronizations
         /// </summary>
@@ -34,6 +48,7 @@ namespace Stock
             {
                 Thread.Sleep(500); // 1/2 second
                 //Call the function ChangeStockValue
+                ChangeStockValue();
             }
         }
         /// <summary>
@@ -42,23 +57,23 @@ namespace Stock
         public void ChangeStockValue()
         {
             var rand = new Random();
-            //CurrentValue += _________________________
-        NumChanges++;
+            CurrentValue += rand.Next(0, MaxChange);
+            NumChanges++;
             if ((CurrentValue - InitialValue) > Threshold)
             {
-                //StockEvent?.Invoke 
+                StockEvent?.Invoke(this, new StockNotification(StockName, CurrentValue, NumChanges));
             }
         }
     }
 
     public class StockBroker
     {
-        public string BrokerName { get; set; }
+        public string BrokerName { get => BrokerName; set => BrokerName = value; }
         public List<Stock> stocks = new List<Stock>();
-        public static ReaderWriterLockSlim myLock = new ReaderWriterLockSlim();
-        readonly string docPath = @"C:\Users\Documents\CECS 475\Lab3_output.txt";
+        public static ReaderWriterLockSlim myLock = new ReaderWriterLockSlim(); readonly string docPath = @"C:\Users\Documents\CECS 475\Lab3_output.txt";
         public string titles = "Broker".PadRight(10) + "Stock".PadRight(15) +
-       "Value".PadRight(10) + "Changes".PadRight(10) + "Date and Time";
+"Value".PadRight(10) + "Changes".PadRight(10) + "Date and Time";
+
         /// <summary>
         /// The stockbroker object
         /// </summary>
@@ -67,15 +82,17 @@ namespace Stock
         {
             BrokerName = brokerName;
         }
+
         /// <summary>
         /// Adds stock objects to the stock list
         /// </summary>
         /// <param name="stock">Stock object</param>
         public void AddStock(Stock stock)
         {
-            //stocks.Add(_______________________);
-            //stock.StockEvent += _________________________________
+            stocks.Add(stock);
+            stock.StockEvent += EventHandler;
         }
+
         /// <summary>
         /// The eventhandler that raises the event of a change
         /// </summary>
@@ -83,20 +100,25 @@ namespace Stock
         /// <param name="e">Event arguments</param>
         void EventHandler(Object sender, EventArgs e)
         {
-            /*try
-            {
-                Stock newStock = (Stock)sender;
-                string statement;
+            myLock.EnterWriteLock();
+            Stock nStock = (Stock)sender;
+            string output = BrokerName.PadRight(10) + nStock.StockName.PadRight(12) + nStock.CurrentValue.ToString().PadRight(7) + nStock.NumChanges;
 
-            } */
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, "output.txt"), true))
+            { 
+                outputFile.WriteLine(output);
+            }
+            Console.WriteLine(output);
+            myLock.ExitWriteLock();
         }
     }
 
     public class StockNotification : EventArgs
     {
-        public string StockName { get; set; }
-        public int CurrentValue { get; set; }
-        public int NumChanges { get; set; }
+
+        public string StockName { get => StockName; set => StockName = value; }
+        public int CurrentValue { get => CurrentValue; set => CurrentValue = value; }
+        public int NumChanges { get => NumChanges; set => NumChanges = value; }
         /// <summary>
         /// Stock notification attributes that are set and changed
         /// </summary>
@@ -105,7 +127,9 @@ namespace Stock
         /// <param name="numChanges">Number of changes the stock goes through</param>
         public StockNotification(string stockName, int currentValue, int numChanges)
         {
-
+            StockName = stockName;
+            CurrentValue = currentValue;
+            NumChanges = numChanges;
         }
     }
 
@@ -117,16 +141,20 @@ namespace Stock
             Stock stock2 = new Stock("Retail", 30, 2, 6);
             Stock stock3 = new Stock("Banking", 90, 4, 10);
             Stock stock4 = new Stock("Commodity", 500, 20, 50);
+
             StockBroker b1 = new StockBroker("Broker 1");
             b1.AddStock(stock1);
             b1.AddStock(stock2);
+
             StockBroker b2 = new StockBroker("Broker 2");
             b2.AddStock(stock1);
             b2.AddStock(stock3);
             b2.AddStock(stock4);
+
             StockBroker b3 = new StockBroker("Broker 3");
             b3.AddStock(stock1);
             b3.AddStock(stock3);
+
             StockBroker b4 = new StockBroker("Broker 4");
             b4.AddStock(stock1);
             b4.AddStock(stock2);
