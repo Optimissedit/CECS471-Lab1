ed781b52b409edea7e6d82138ae1a2c5c8040b3f
zackhunter <zack0hunter@gmail.com>
2021-09-15 12:10:20 -0700

    Update Program.cs

--- CECS471 Lab1/Program.cs
+++ CECS471 Lab1/Program.cs
@@ -65,7 +65,6 @@ namespace Stock
             var rand = new Random();
             CurrentValue += rand.Next(-MaxChange - 1, MaxChange + 1);
             NumChanges++;
-
             if ((CurrentValue - InitialValue) > Threshold)
             {
                 //Console.WriteLine("Hit on Stock " + StockName);
@@ -139,7 +138,6 @@ namespace Stock
 
     public class StockNotification : EventArgs
     {
-
         public string StockName { get; set; }
         public int CurrentValue { get; set; }
         public int NumChanges { get; set; }
@@ -147,7 +145,7 @@ namespace Stock
         /// Stock notification attributes that are set and changed
         /// </summary>
         /// <param name="stockName">Name of stock</param>
-        /// <param name="currentValue">Current vallue of the stock</param>
+        /// <param name="currentValue">Current value of the stock</param>
         /// <param name="numChanges">Number of changes the stock goes through</param>
         public StockNotification(string stockName, int currentValue, int numChanges)
         {
